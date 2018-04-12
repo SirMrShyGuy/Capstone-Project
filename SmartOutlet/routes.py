ad45410541b276fae6197ce9ec0fe333921b2502
Preston Bryan <pbryan3630@gmail.com>
2018-04-12 03:25:10 -0500

    update webapp

--- SmartOutlet/routes.py
+++ SmartOutlet/routes.py
@@ -1,15 +1,17 @@
-from flask import render_template
+from flask import render_template, Response
 from SmartOutlet import app
 import RPi.GPIO as GPIO
+import pygal
+import sqlite3
 
 GPIO.setmode(GPIO.BCM)
 
 #Outlet Info
 outletInfo = [
-    {"id": 0, "name": "Outlet 1", "pinNum": 5, "state": GPIO.LOW},
-    {"id": 1, "name": "Outlet 2", "pinNum": 6, "state": GPIO.LOW},
-    {"id": 2, "name": "Outlet 3", "pinNum": 13,"state": GPIO.LOW},
-    {"id": 3, "name": "Outlet 4", "pinNum": 19,"state": GPIO.LOW}
+    {"id": 0, "name": "Outlet1", "pinNum": 5, "state": GPIO.LOW},
+    {"id": 1, "name": "Outlet2", "pinNum": 6, "state": GPIO.LOW},
+    {"id": 2, "name": "Outlet3", "pinNum": 13,"state": GPIO.LOW},
+    {"id": 3, "name": "Outlet4", "pinNum": 19,"state": GPIO.LOW}
     ]
 
 #Start GPIO setup
@@ -50,3 +52,11 @@ def action(outletId, action):
             'outletInfo': outletInfo
             }
     return render_template("index.html", **templateData)
+
+#-creates images for graphs
+@app.route("/<outletId>/graph")
+def graph(outletId):
+    chart = pygal.Line()
+    chart.title = outletInfo[int(outletId)]["name"] + " Power Usage"
+    chart.add(outletInfo[int(outletId)]["name"], [1,2,3,4,5,6,7,8,9,10])
+    return Response(response=chart.render(), content_type="image/svg+xml")
--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 from SmartOutlet import app
 
-app.run(debug = True, host = "0.0.0.0")
+import threading
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="[%(levelname)-7s] (%(threadName)-11s) %(message)s",
+                    )
+
+# separate thread 
+def database_service():
+    logging.debug("Starting")
+    print("this is where the database will start")
+    logging.debug("Exiting")
+    
+database_t = threading.Thread(name="Database", target=database_service)
+database_t.start()
+
+app.run(debug=True, host="0.0.0.0")
